1ae594801e612ced2449fdb022c513fab4efa275
gary <gary.white@lodgit.com>
2021-12-09 13:06:08 +0100

    boilerplate

---
+++ index.js
@@ -0,0 +1,10 @@
+function correct(string)
+{
+	// your code here
+}
+
+console.log(correct("L0ND0N")); // return 'LONDON'
+console.log(correct("DUBL1N")); // return 'DUBLIN'
+console.log(correct("51NGAP0RE")); // return "SINGAPORE"
+console.log(correct("BUDAPE5T")); // return "BUDAPEST"
+console.log(correct("PAR15")); // return 'PARIS'
